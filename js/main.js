bdeb3386e954d253389343158046c10456eacebd
young <532265997@qq.com>
2016-05-24 17:12:09 +0800

    添加自动提示近期考试功能

--- js/main.js
+++ js/main.js
@@ -17,8 +17,10 @@
 		} else {
 			setTimeout(function() {
 				plus.navigator.closeSplashscreen();
-			}, 2000);
+			}, 1500);
 		}
+
+		examHint();
 
 		M.preload({
 			id: 'parent',
@@ -34,7 +36,7 @@
 		M('#main').on('tap', 'li', function(e) {
 			var self = this,
 				title = $(this).find('div').text();
-				
+
 			if (!parentPage) {
 				parentPage = plus.webview.getWebviewById('parent');
 			}
@@ -47,6 +49,14 @@
 			M.openWindow({
 				id: 'parent'
 			});
+		});
+
+		M('#popover').on('change', 'input', function() {
+			if (this.checked) {
+				plus.storage.setItem('isHint', 'false-' + (+new Date));
+			} else {
+				plus.storage.setItem('isHint', 'true');
+			}
 		});
 
 		var backButtonPress = 0;
@@ -63,5 +73,58 @@
 			return false;
 		};
 	});
+
+	M('#popover').on('tap', 'button', function(e) {
+		M('#popover').popover('hide');
+	});
+
+	function examHint() {
+		var isHint = plus.storage.getItem('isHint');
+		if (isHint && isHint.indexOf('false') > -1) {
+			if (new Date - isHint.split('-')[1] >= 24 * 60 * 60 * 1000) {
+				plus.storage.removeItem('isHint');
+			} else {
+				return;
+			}
+		}
+
+		var newArr = [],
+			exam = T.getData('exam');
+
+		$.each(exam, function() {
+			var $this = $(this),
+				examDetailTime = $this[2];
+
+			if (examDetailTime == '&nbsp;') return false;
+
+			var examDate = examDetailTime.match(/\((.*?)\)/)[1],
+				examTime = examDetailTime.split(' ')[1].split('-')[0];
+
+			if (checkDate(examDate + ' ' + examTime)) {
+				newArr.push({
+					time: examDetailTime,
+					place: $this[3],
+					courseName: $this[0]
+				});
+			}
+		});
+
+		if (newArr.length > 0) {
+			$('#popover').html(template('exam-hint-tpl', {
+				dataList: newArr,
+				count: newArr.length
+			}));
+
+			M('#popover').popover('show');
+		}
+	}
+
+	function checkDate(date) {
+		var distance = new Date(date) - new Date('2016-5-22 22:00');
+		if (distance >= 0 && (distance / 1000 / 60 / 60) < 24) {
+			return true;
+		}
+		return false;
+	}
 
 }(mui, Zepto, window));
